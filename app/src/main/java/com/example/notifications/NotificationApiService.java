aa6be2c82dc248580a27e6ebc6c8a2350be3ea38
ahmadmahoud <72008832+ahmadmahoud@users.noreply.github.com>
2021-06-06 11:32:35 +0200

    Update NotificationApiService.java

--- app/src/main/java/com/example/notifications/NotificationApiService.java
+++ app/src/main/java/com/example/notifications/NotificationApiService.java
@@ -11,7 +11,7 @@ public interface NotificationApiService {
 
 
     @Headers({
-            "Authorization: key="+"AAAAdfacxxc:APA91bGE5bkZBLYJRyrUzQc_8_KTRZ2Z8KN9zPP380td9NQukts_4tFpFF8_JI0d87kHqpfxKOxT22O4-r2hBvSZoFbMgNQxZW6flGvbDcfIDpEIGYzeaYs7SlhmyoiET-SVwoGwCMYJ" ,
+            "Authorization: key="+"paste your token here" ,
             "Content-Type: application/json"
     })
     @POST("fcm/send")
